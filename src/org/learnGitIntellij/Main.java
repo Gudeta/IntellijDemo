e702516898ebd2f95c26a323ee19a65837eda84a
Gudeta <gumodex@gmail.com>
2021-03-27 17:17:20 -0500

    Intellij Git Practice....Modified!Git..

--- src/org/learnGitIntellij/Main.java
+++ src/org/learnGitIntellij/Main.java
@@ -1,9 +1,30 @@
 package org.learnGitIntellij;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+
+    List<String> voterList=new ArrayList<>();
+
+    public Main(){
+        voterList.add("v1");
+        voterList.add("v2");
+        voterList.add("v3");
+        voterList.add("v4");
+        voterList.add("v5");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello Git ^ Intellij Modified!!");
-        System.out.println("Git");
+        Main voters=new Main();
+        System.out.println(voters.voterList);
+        removeVoter(voters);
+
+
+    }
+
+    private static void removeVoter(Main voters) {
+        voters.voterList.remove("v1");
+        System.out.println(voters.voterList);
     }
 }
