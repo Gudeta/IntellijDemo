4db8d121b9b3a030bf75633dd8855d59e018cd53
Gudeta <gumodex@gmail.com>
2021-03-27 16:42:46 -0500

    Intellij Git Practice....Modified!Git

---
+++ src/org/learnGitIntellij/Main.java
@@ -0,0 +1,9 @@
+package org.learnGitIntellij;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git ^ Intellij Modified!!");
+        System.out.println("Git");
+    }
+}
